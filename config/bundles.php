312e4ea35c1e1ea25dd1ffa1cde94bf82d77fc0e
k911 <konradobal@gmail.com>
2017-09-25 23:13:57 +0200

    Disable easyadmin for production env

--- config/bundles.php
+++ config/bundles.php
@@ -8,11 +8,11 @@ return [
     'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
     'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
     'ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle' => ['all' => true],
-    'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle' => ['dev' => true, 'test' => true],
-    'JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle' => ['all' => true],
     'FOS\UserBundle\FOSUserBundle' => ['all' => true],
     'Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle' => ['all' => true],
     'Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle' => ['all' => true],
     'Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle' => ['dev' => true, 'test' => true],
     'Nelmio\CorsBundle\NelmioCorsBundle' => ['all' => true],
+    'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle' => ['dev' => true, 'test' => true],
+    'JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle' => ['dev' => true, 'test' => true],
 ];
