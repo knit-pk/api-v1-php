a039936878ededf9e53d40718a21e2f126555b53
k911 <konradobal@gmail.com>
2017-10-05 22:19:47 +0200

    Update version and mydevil deploy

--- mydevil.sh
+++ mydevil.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+rm -rf .git vendor logs public/bundles config/jwt var supervisord.pid
+
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
@@ -10,4 +12,5 @@ php71 composer.phar install -o --no-scripts
 php71 bin/console cache:clear
 php71 bin/console cache:warmup
 gmake generate-jwt-keys
-php71 bin/console assets:install --symlink --relative ../public_html
+php71 bin/console assets:install --symlink --relative public
+cp -a public/* ../public_html
