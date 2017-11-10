204de5cebf189229b02d51700db0c31fa19e946f
k911 <konradobal@gmail.com>
2017-11-10 14:42:02 +0100

    Configure Elastic Stack

--- config/packages/dev/monolog.php
+++ config/packages/dev/monolog.php
@@ -4,30 +4,25 @@ use Symfony\Component\Config\Resource\ClassExistenceResource;
 use Symfony\Component\Console\Application;
 
 $handlers = [
-    'main' => [
-        'type' => 'stream',
-        'path' => '%kernel.logs_dir%/%kernel.environment%.log',
-        'level' => 'debug',
+    'main'          => [
+        'type'     => 'stream',
+        'path'     => '%kernel.logs_dir%/%kernel.environment%.log',
+        'level'    => 'debug',
         'channels' => ['!event'],
     ],
-    // uncomment to get logging in your browser
-    // you may have to allow bigger header sizes in your Web server configuration
-    //'firephp' => [
-    //    'type' => 'firephp',
-    //    'level' => 'info',
-    //],
-    //'chromephp' => [
-    //    'type' => 'chromephp',
-    //    'level' => 'info',
-    //],
+    'redis'         => [
+        'id'    => 'redis_handler',
+        'type'  => 'service',
+        'level' => 'info',
+    ],
 ];
 
 $container->addResource(new ClassExistenceResource(Application::class));
 if (class_exists(Application::class)) {
     $handlers['console'] = [
-        'type' => 'console',
+        'type'                   => 'console',
         'process_psr_3_messages' => false,
-        'channels' => ['!event', '!doctrine', '!console'],
+        'channels'               => ['!event', '!doctrine', '!console'],
     ];
 }
 
