2ad23c909a1dbeb85bb375f2d1ba23ae3f5e1ee0
k911 <konradobal@gmail.com>
2017-11-25 00:38:10 +0100

    Optimize User Entity (Create own instead of extending FOSUser's)

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -48,7 +48,7 @@ class Tag
     /**
      * @var Uuid
      *
-     * @ORM\Id
+     * @ORM\Id()
      * @ORM\Column(type="uuid")
      * @ORM\GeneratedValue(strategy="CUSTOM")
      * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
