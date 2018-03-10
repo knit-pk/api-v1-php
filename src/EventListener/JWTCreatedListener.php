cf2e57c3622c3e4dbd84ce5252e2e737b5165cb2
Konrad Obal <konradobal@gmail.com>
2018-03-10 23:49:31 +0100

    Add JWTCreatedListener test (#56)

--- src/EventListener/JWTCreatedListener.php
+++ src/EventListener/JWTCreatedListener.php
@@ -4,15 +4,24 @@ declare(strict_types=1);
 
 namespace App\EventListener;
 
-use App\Entity\User;
+use App\Security\Exception\SecurityException;
+use App\Security\User\UserInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
 
 final class JWTCreatedListener
 {
+    /**
+     * @param \Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent $event
+     *
+     * @throws \App\Security\Exception\SecurityException
+     */
     public function onJWTCreated(JWTCreatedEvent $event): void
     {
-        /** @var User $user */
         $user = $event->getUser();
+
+        if (!$user instanceof UserInterface) {
+            throw new SecurityException('JWTCreated event must provide user object implementing local UserInterface', 500);
+        }
 
         $payload = $event->getData();
         $payload['id'] = (string) $user->getId();
