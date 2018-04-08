fec9a2a8f96dc8a978ac5037e99c035a3d63da39
k911 <konradobal@gmail.com>
2018-04-08 15:21:15 +0200

    Build :latest and :standalone tags

--- deploy/docker-app-push.sh
+++ deploy/docker-app-push.sh
@@ -3,7 +3,8 @@
 DOCKER_TAG=$(echo ${TRAVIS_TAG} | sed -E 's~^v(.*)~\1~')
 
 # Docker Hub
-docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+
 docker build . -t knitpk/api:${DOCKER_TAG}
 docker push knitpk/api:${DOCKER_TAG}
 
@@ -13,3 +14,9 @@ docker build -f Dockerfile.standalone . -t knitpk/api:${DOCKER_TAG}-standalone \
     --build-arg KNIT_API_URL=https://d15e2ckuuchn46.cloudfront.net
 
 docker push knitpk/api:${DOCKER_TAG}-standalone
+
+docker tag knitpk/api:${DOCKER_TAG} knitpk/api:latest
+docker push knitpk/api:latest
+
+docker tag knitpk/api:${DOCKER_TAG}-standalone knitpk/api:standalone
+docker push knitpk/api:standalone
