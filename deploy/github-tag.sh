cc62206a1f2bf5a040c014654c5f04849fbd3f7e
k911 <konradobal@gmail.com>
2018-04-08 15:03:43 +0200

    Remove possibility to override git tag

--- deploy/github-tag.sh
+++ deploy/github-tag.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-git tag ${APP_VERSION} --force
+git tag ${APP_VERSION}
 
 git remote add authorized https://travis:${GH_TOKEN}@github.com/knit-pk/api-v1-php.git
-git push authorized ${APP_VERSION} --force
+git push authorized ${APP_VERSION}
