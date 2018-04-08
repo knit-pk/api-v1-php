9b5f04bef391d5ab2aa20b4d5441c14ba030e925
k911 <konradobal@gmail.com>
2018-04-08 15:59:39 +0200

    Use cache from option to speed up builds

--- deploy/docker-app-push.sh
+++ deploy/docker-app-push.sh
@@ -6,6 +6,7 @@ DOCKER_TAG=$(echo ${TRAVIS_TAG} | sed -E 's~^v(.*)~\1~')
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
 docker build . \
+    --cache-from knitpk/api:latest \
     --tag knitpk/api:${DOCKER_TAG} \
     --tag knitpk/api:latest
 
@@ -13,10 +14,11 @@ docker push knitpk/api:${DOCKER_TAG}
 docker push knitpk/api:latest
 
 docker build -f Dockerfile.standalone . \
+    --cache-from knitpk/api:standalone \
     --tag knitpk/api:${DOCKER_TAG}-standalone \
     --tag knitpk/api:standalone \
     --build-arg KNIT_API_TAG=${DOCKER_TAG} \
-    --build-arg KNIT_API_ADMIN_TAG=0.3.1-dev \
+    --build-arg KNIT_API_ADMIN_TAG=latest \
     --build-arg KNIT_API_URL=https://d15e2ckuuchn46.cloudfront.net
 
 docker push knitpk/api:${DOCKER_TAG}-standalone
