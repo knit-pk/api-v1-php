5595069e62feb141be1d2619321a4692447c235a
Konrad Obal <konradobal@gmail.com>
2018-07-05 20:52:18 +0200

    ci(travis): Automate release process (#138)
    Create changelog (from commit messages), tag version, push to git repository and make a GitHub
    release

--- deploy/github-tag.sh
+++ deploy/github-tag.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env sh
 
+if [[ $(git name-rev --name-only --tags HEAD) = "$APP_VERSION" ]]; then
+  echo "Already tagged. Skipping.."
+  exit
+fi
+
+conventional-changelog -p angular -i CHANGELOG.md -s -r 0
+git add CHANGELOG.md
+git commit -m "chore(release): ${APP_VERSION} :tada:
+
+$(conventional-changelog)
+"
+
 git tag ${APP_VERSION}
 
 git remote add authorized https://travis:${GH_TOKEN}@github.com/knit-pk/api-v1-php.git
-git push authorized ${APP_VERSION}
+git push authorized HEAD:master --tags
+git push authorized HEAD:develop
+
+conventional-github-releaser -p angular -t ${GH_TOKEN}
